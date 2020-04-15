1b100cc1fdeb655b473e0a74d45867ec8f3731c7
wtbdev <wtbstudio@protonmail.com>
2020-04-15 19:51:19 +0800

    update logger

--- src/io/rukkit/Rukkit.java
+++ src/io/rukkit/Rukkit.java
@@ -1,9 +1,10 @@
 package io.rukkit;
-import java.util.logging.*;
+import io.rukkit.util.*;
 
 public class Rukkit
 {
+	private static final Logger log = new Logger("Main");
 	public static void main(String args[]){
-		
+		log.i("Welcome to use Rukkit Server!");
 	}
 }
--- src/io/rukkit/util/Logger.java
+++ src/io/rukkit/util/Logger.java
@@ -1,5 +1,8 @@
 package io.rukkit.util;
 
+import java.util.*;
+import io.rukkit.*;
+
 public class Logger
 {
 	private String tag;
@@ -9,18 +12,24 @@ public class Logger
 	}
 	
 	public void i(Object obj){
-		
+		Date d = new Date();
+		System.out.printf("%tF %tT [INFO][%s] %s\n", d, d, tag, obj.toString());
 	}
 	
 	public void w(Object obj){
-		
+		Date d = new Date();
+		System.out.printf("\033[33m%tF %tT [WARN][%s] %s\033[0m\n", d, d, tag, obj.toString());
 	}
 	
 	public void e(Object obj){
-		
+		Date d = new Date();
+		System.out.printf("\033[31m%tF %tT [ERROR][%s] %s\033[0m\n", d, d, tag, obj.toString());
 	}
 	
 	public void d(Object obj){
-		
+		if(ServerProperties.isDebug){
+			Date d = new Date();
+			System.out.printf("\033[32m%tF %tT [DEBUG][%s] %s\033[0m\n", d, d, tag, obj.toString());
+		}
 	}
 }
